46a3069e471901667a48e395ec8d7abec64732f6
Antillion <antillion@debianPC.home>
2020-08-10 01:47:23 -0300

    Gnostic Section Added + Scripts

---
+++ js/header.js
@@ -0,0 +1,8 @@
+let subtitleList = ["The Webbed Site", "MORTIS", "Also try Minecraft!", "IDKFA", "I know you did it", "It's Dare", "Pablo hates Honey", 
+"GNU/Linux or GNU + Linux", "Java sucks", "Анархия-мама", "1312", "This isn't real", "Zimbabwe", "Fully Funkomatic", "As seen on TV!",
+"Zalgo!", "Ahre loco", "And you don't seem to understand", "A shame you seemed an honest man", "Serial Experiments inside!", "Annie May",
+"Antlion", "sudo apt-get install life", "Post-Punk Vibes", "Rainbow Bends", "Leeneks", "Let's Rock", "Antillion says Trans Rights!", "Who dis",
+"Praise the Monad!", "Who tf are you", "Look Gordon, Ropes!", "Hello Gordon", "I use Arch btw", "No Gamers Allowed"];
+
+ // Set Subtitle
+document.getElementById("headSubtitle").innerHTML = subtitleList[Math.floor(Math.random()*subtitleList.length)];
